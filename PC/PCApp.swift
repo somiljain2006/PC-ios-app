3f2ee8bb3d2ac57da9e68b284247229157340ddf
somiljain2006 <somil16022006@gmail.com>
2026-04-10 16:35:37 +0530

    Add Splash screen

---
+++ PC/PCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCApp.swift
+//  PC
+//
+//  Created by somil jain on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
